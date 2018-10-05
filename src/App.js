ba820572e0f08b4d98028b0c3843833203570a81
mattrudin <matthias.rudin@live.com>
2018-10-05 11:55:20 +0200

    Added async function for login, needs further testing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import { clearStorage, getCode, oauthLogin, getAccessToken, shortenCode, getArticles } from './utils/oauth';
+import { loginFunction, clearStorage, getCode, oauthLogin, getAccessToken, shortenCode, getArticles } from './utils/oauth';
 
 class App extends Component {
   constructor(props) {
@@ -11,18 +11,20 @@ class App extends Component {
     }
   }
 
-  componentDidMount() {
+  /* componentDidMount() {
     this.timerID = setInterval(
       () => getCode(),
       1000
     );
-  }
+  } */
 
   render() {
     return (
       <div className="App">
+        <button onClick={loginFunction}>Login to Bexio</button>
         <button onClick={clearStorage}>Clear Storage</button>
         <button onClick={oauthLogin}>Get Code</button>
+        <button onClick={getCode}>Save Code</button>
         <button onClick={shortenCode}>Shorten Code</button>
         <button onClick={getAccessToken}>Get AccessToken</button>
         <button onClick={getArticles}>Get Articles</button>
--- src/utils/oauth.js
+++ src/utils/oauth.js
@@ -1,19 +1,18 @@
 import {JSO} from 'jso';
 
-const clientID = 'XXXXXXXXXXXXXXXXXXXXXXXXX';
-const clientSecret = 'XXXXXXXXXXXXXXXXXXXXXXXXX';
+const clientID = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
+const clientSecret = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
 
-let jso = new JSO({
-	providerID: "bexio",
-	client_id: clientID,
-	redirect_uri: "http://localhost:3000/",
-	authorization: "https://office.bexio.com/oauth/authorize/",
-    scopes: { request: ["article_show"]},
-    response_type: 'code',
-	client_secret: clientSecret,
-    token: "https://office.bexio.com/oauth/access_token/",
-    request: { state: '1234567890'}
-});
+export async function loginFunction() {
+   // setLoginData(clientIDinput, clientSecretinput);
+    clearStorage();
+    await oauthLogin();
+    await getCode();
+    await shortenCode();
+    await getAccessToken();
+    alert('Async function complete');
+}
+
 
 export const clearStorage = () => {
     localStorage.clear();
@@ -22,32 +21,48 @@ export const clearStorage = () => {
 export const getCode = () => {
     let accessCode = '';
     if(!accessCode) {
-        accessCode = window.location.href.match(/code=([^&]*)/);
-        localStorage.setItem('codeLong', accessCode);
+        const codeLong = copyCodeFromUrl();
+        accessCode = shortenCode(codeLong);
     } else {
         return
     }
     return accessCode;
 }
 
+
 export const oauthLogin = () => {
+    let jso = new JSO({
+        providerID: "bexio",
+        client_id: clientID,
+        redirect_uri: "http://localhost:3000/",
+        authorization: "https://office.bexio.com/oauth/authorize/",
+        scopes: { request: ["article_show"]},
+        response_type: 'code',
+        client_secret: clientSecret,
+        token: "https://office.bexio.com/oauth/access_token/",
+        request: { state: '1234567890'}
+    });
     jso.getToken();
 }
 
-export const shortenCode = () => {
-    const codeLong = localStorage.getItem('codeLong');
-    const codeShort = codeLong.match("code=(.*),");
-    localStorage.setItem('code', codeShort[1]);
+const copyCodeFromUrl = () => {
+    const accessCode = window.location.href.match(/code=([^&]*)/);
+    return accessCode;
 }
 
-export const getAccessToken = () => {
+const shortenCode = (codeLong) => {
+    const codeShort = codeLong.match("code=(.*),");
+    const code = codeShort[1];
+    return code;
+}
+
+export const getAccessToken = (code) => {
     
     //no 'access-control-allow-origin' header is present on the requested resource.
 
     let http = new XMLHttpRequest();
     const url = 'https://office.bexio.com/oauth/access_token/';
     const redirect_uri = 'http://localhost:3000/';
-    const code = localStorage.getItem('code');
 
     const params = `client_id=${clientID}&redirect_uri=${redirect_uri}&client_secret=${clientSecret}&code=${code}`;
     http.open('POST', url, true);
